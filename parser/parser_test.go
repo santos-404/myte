ecad064f59f5542eca7befaf56a01def7559d588
Javier Santos <javsanmar5@alum.us.es>
2025-07-03 19:42:18 +0200

    Handle parsing the return statement (#40)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -93,3 +93,35 @@ func checkParserErrors(t *testing.T, p *Parser) {
 	}
 	t.FailNow()
 }
+
+
+func TestReturnStatement(t *testing.T) {
+	input := `
+return 12;
+
+return foo();
+return bar;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.returnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+			returnStmt.TokenLiteral())
+		}
+	}
+}
--- parser/statements.go
+++ parser/statements.go
@@ -10,10 +10,13 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currentToken.Type {
 	case token.VAR:
 		return p.parseVarStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
 }
+
 
 func (p *Parser) parseVarStatement() *ast.VarStatement {
 	stmt := &ast.VarStatement{Token: p.currentToken}
@@ -25,11 +28,26 @@ func (p *Parser) parseVarStatement() *ast.VarStatement {
 	stmt.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
 	
 	// TODO: We don't have to wait for a semicolon, but for an assignment if the var
-	// is being initialized. If it's just being declared, then a semicolon is ok.
+	// is being initialized. If it's just being declared, then a semicolon is ok.I 
 	for p.currentToken.Type != token.SEMICOLON {
 		p.nextToken()
 	}
 
 	return stmt 
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.currentToken}
+
+	// We don't do anything about ensuring the following token is whatever
+	// because I don't think it makes any sense. It might be a lot of things
+	p.nextToken()
+	
+	// TODO: Once again, we shouldn't skip everything til a semicolon is found
+	for p.currentToken.Type != token.SEMICOLON {
+		p.nextToken()
+	}
+
+	return stmt
 }
 

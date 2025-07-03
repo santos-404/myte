7fa1a099ae7301912e0ea10837c89a7fca0641e6
Javier Santos <javsanmar5@alum.us.es>
2025-07-03 17:35:15 +0200

    Test errors (#34)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -28,6 +28,8 @@ var foobar = 123.45;
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -77,4 +79,17 @@ func testVarStatement(t *testing.T, statement ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()	
+	if len(errors) == 0 {
+		return 
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)	
+	}
+	t.FailNow()
 }

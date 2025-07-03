c9ba1e0dcf3ae446662c3d1971a10409af984986
Javier Santos <javsanmar5@alum.us.es>
2025-07-03 11:53:20 +0200

    Extend string methods on the ast interfaces (#28)

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/santos-404/myte/token"
+import (
+	"bytes"
+
+	"github.com/santos-404/myte/token"
+)
 
 type Node interface {
 	TokenLiteral() string  // This is here only for debugging reasons
+	String() string
 }
 
 type Statement interface {
@@ -29,6 +34,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 
 type VarStatement struct {
 	Token token.Token  // This is the token.VAR
@@ -38,6 +53,21 @@ type VarStatement struct {
 
 func (vs *VarStatement) statementNode()			{}
 func (vs *VarStatement) TokenLiteral() string 	{ return vs.Token.Literal }
+func (vs *VarStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(vs.TokenLiteral() + " ")
+	out.WriteString(vs.Name.String())
+	out.WriteString(" = ")
+
+	if vs.Value != nil {
+		out.WriteString(vs.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 
 type Identifier struct {
@@ -47,4 +77,5 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()			{}
 func (i *Identifier) TokenLiteral() string 		{ return i.Token.Literal }
+func (i *Identifier) String() string 			{ return i.Value }
 

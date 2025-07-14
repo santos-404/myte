6b130ab66602b5a207a14ea3826ffaa88c465d30
Javier Santos <javsanmar5@alum.us.es>
2025-07-14 18:35:33 +0200

    Test handling integer expressions (#39)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -154,3 +154,31 @@ func TestIdentifierExpressions(t *testing.T) {
 		t.Fatalf("ident.Value not %s. got=%s", "foobar", ident.Value)	
 	}
 }
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not 1 statement. got=%d", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an ast.ExpressionStatemnt. got=%T", 
+			program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral) 
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)	
+	}
+	if literal.Value != 5 {
+		t.Fatalf("ident.Value not %s. got=%d", "5", literal.Value)	
+	}
+}

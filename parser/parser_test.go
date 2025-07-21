ec06fdba12c2b868f42fcb2eb2b0bfa9e24002bc
Javier Santos <javsanmar5@alum.us.es>
2025-07-21 18:44:11 +0200

    Test if expression parsing with and without parenthesis (#53)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -568,9 +568,54 @@ func testInfixExpression(
 	return true
 }
 
+func TestIfExpressionWithParen(t *testing.T) {
+	input := "if (x < y) { x }"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+			len(program.Statements))
+	}
+	
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an ast.ExpressionStatemnt. got=%T", 
+			program.Statements[0])
+	}
+
+	exp, ok := stmt.Expression.(*ast.IfExpression) 
+	if !ok {
+		t.Fatalf("exp not *ast.IfExpression. got=%T", stmt.Expression)	
+	}
+
+	testInfixExpression(t, exp.Condition, "x", "<", "y")
+
+	if len(exp.Consequence.Statements) != 1 {
+		t.Errorf("consequence is not 1 statements. got=%d", len(exp.Consequence.Statements))
+	}
+
+	consequence, ok := exp.Consequence.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("Statments[0] is not an ast.ExpressionStatement. got=%T",
+			exp.Consequence.Statements[0])
+	}
+
+	if !testIdentifier(t, consequence.Expression, "x") {
+		return
+	}
+
+	if exp.Alternative != nil {
+		t.Errorf("exp.Alternative.Statements was not nil. got=%+v", exp.Alternative)
+	}
+}
 
 func TestIfExpression(t *testing.T) {
-	input := "if (x < y) { x }"
+	input := "if x < y { x }"
 
 	l := lexer.New(input)
 	p := New(l)

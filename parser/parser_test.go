9650ae9f81dcf1f697360508c0f44c1f8ad7bb45
Javier Santos <javsanmar5@alum.us.es>
2025-08-02 19:24:54 +0200

    Add test for call expression parsing (#55)

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -528,6 +528,7 @@ func testLiteralExpression(
 	case int64:
 		return testIntegerLiteral(t, exp, v)
 	case string:
+		fmt.Println(exp, v)
 		return testIdentifier(t, exp, v)	
 	case bool:
 		return testBooleanLiteral(t, exp, v)
@@ -844,5 +845,39 @@ func TestForLoopLiteralFormat1(t *testing.T) {
 	}
 
 
+}
+
+func TestCallExpression(t *testing.T) {
+	input := "foo(3, 1 + 2, bar);"
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+			len(program.Statements))
+	}
+	
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an ast.ExpressionStatemnt. got=%T", 
+			program.Statements[0])
+	}	
+
+	exp, ok := stmt.Expression.(*ast.CallExpression) 
+	if !ok {
+		t.Fatalf("stmt.Expression not *ast.CallExpression. got=%T", stmt.Expression)	
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Errorf("function literal parameter wrong. want=3. got=%d", len(exp.Arguments))
+	}
+	
+	testLiteralExpression(t, exp.Arguments[0], 3)
+	testInfixExpression(t, exp.Arguments[1], 1, "+", 2)
+	testLiteralExpression(t, exp.Arguments[2], "bar")
 }
 

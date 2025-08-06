2386452433539c2de172c9d45828fcdaf3519081
Javier Santos <javsanmar5@alum.us.es>
2025-08-06 12:52:21 +0200

    Log tag when running Myte (#27)

--- main.go
+++ main.go
@@ -8,12 +8,22 @@ import (
 	"github.com/santos-404/myte/repl"
 )
 
+const ASCII_ART = `░▒▓██████████████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░   ░▒▓█▓▒░   ░▒▓██████▓▒░   
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓████████▓▒░ 
+                                                                `
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println(ASCII_ART)
 	fmt.Printf("Hi there %s!, this is the Myte programming language!\n", 
 		user.Username)
 	fmt.Println("Feel free to type in commands")
